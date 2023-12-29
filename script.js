c886d77137ea58bfef6f8b12bbf3124f5e658aa7
Aritra-3-Roy <aritra.briliant@gmail.com>
2023-12-30 00:38:49 +0530

    first commit

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 function showKeyCodes(e) {
     const insert = document.getElementById('insert');
-    insert.innerHTML ='';
-
-    const KeyCodes = {
-        'e.key' : e.key ===' ' ? 'Space' : e.key,
-        'e.keyCode' : e.keyCode,
-        'e.code' : e.code, 
+    insert.innerHTML = '';
+  
+    const keyCodes = {
+      'e.key': e.key === ' ' ? 'Space' : e.key,
+      'e.keyCode': e.keyCode,
+      'e.code': e.code,
     };
-
-    for(let key in KeyCodes)
-    {
-        const div = document.createElement('div');
-        div.className = 'key';
-        const small = document.createElement('small');
-
-        const keyText = document.createTextNode(key);
-        //const valueText = document.createTextNode(KeyCodes[key]);
-
-        small.appendChild(keyText);
-        //div.appendChild(valueText);
-        div.appendChild(small);
-        insert.appendChild(div);
+  
+    for (let key in keyCodes) {
+      const div = document.createElement('div');
+      div.className = 'key';
+      const small = document.createElement('small');
+  
+      const keyText = document.createTextNode(key);
+      const valueText = document.createTextNode(keyCodes[key]);
+  
+      small.appendChild(keyText);
+      div.appendChild(valueText);
+      div.appendChild(small);
+  
+      insert.appendChild(div);
     }
-}
-window.addEventListener('keypress', showKeyCodes);
+  }
+  
+  window.addEventListener('keydown', showKeyCodes);
